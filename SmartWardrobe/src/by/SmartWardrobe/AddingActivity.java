8d9ea63c3192b438955152b90b2f994e68933b3d
Nastassia Mitskevich <nastassia.mitskevich@gmail.com>
2014-10-18 23:28:45 +0300

    Adding Activity edited. Actions are added.

--- SmartWardrobe/src/by/SmartWardrobe/AddingActivity.java
+++ SmartWardrobe/src/by/SmartWardrobe/AddingActivity.java
@@ -2,25 +2,54 @@ package by.SmartWardrobe;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
 
 import by.idea.SmartWardrobe.R;
 import main.constants.Category;
+import main.wardrobe.entity.Apparel;
+import main.wardrobe.service.WardrobeManager;
 
 public class AddingActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.adding_activity);
-        //TODO сделать добавление предмета в коллекцию + вызов камеры!!!
+        //TODO сделать добавление предмета в коллекцию
 
-        //startActivity(new Intent(Intent.ACTION_CAMERA_BUTTON));
+        ImageView imV = (ImageView)findViewById(R.id.imageViewAdd);
+        imV.setImageResource(by.idea.SmartWardrobe.R.drawable.ex);
 
+        final Spinner sp  = (Spinner)findViewById(R.id.spinnerCatAdd);
+        final SpinnerAdapter spAdapter = new ArrayAdapter<String>(this, by.idea.SmartWardrobe.R.layout.list_item_category, Category.getCategories());
+        sp.setAdapter(spAdapter);
 
+        final EditText edtName = (EditText)findViewById(R.id.editTextNameAdd);
+        final EditText edtMinT = (EditText)findViewById(R.id.editTextMinTAdd);
+        final EditText edtMaxT = (EditText)findViewById(R.id.editTextMaxTAdd);
+        final EditText edtDescr = (EditText)findViewById(R.id.editTextDescrAdd);
+
+        Button btn = (Button)findViewById(R.id.buttonToWardrobeAdd);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String cat = sp.getSelectedItem().toString();
+                String name = edtName.getText().toString();
+                String minT = edtMinT.getText().toString();
+                String maxT = edtMaxT.getText().toString();
+                String descr = edtDescr.getText().toString();
+
+                Apparel newApparel = new Apparel(name, BitmapFactory.decodeResource(getResources(), R.drawable.ex), Category.getByType(cat), 3, descr);
+
+            }
+        });
     }
 }
