9565be2fecaf22189e1460d635ef7e1e818880a9
TDiva <tanushadomanova@gmail.com>
2014-10-18 21:10:40 +0300

    Category names

--- SmartWardrobe/src/main/constants/Category.java
+++ SmartWardrobe/src/main/constants/Category.java
@@ -4,33 +4,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 public enum Category {
-	DRESS("Платья"), TROUSERS("�����"), SHIRT("�������"), TSHIRTS("�����"), SWEATER(
-			"�������"), ACCESSORIES("����������"), OTHER("Другое");
+    DRESS("Платья"),
+    TROUSERS("Брюки"),
+    SHIRT("Рубашки"),
+    TSHIRTS("Футболки"),
+    SWEATER("Свитера"),
+    ACCESSORIES("Аксессуары"),
+    OTHER("Другое");
 
-	private String type;
+    private String type;
 
-	Category(String type) {
-		this.type = type;
-	}
+    Category(String type) {
+        this.type = type;
+    }
 
-	public String getType() {
-		return type;
-	}
+    public String getType() {
+        return type;
+    }
 
-	public static Category getByType(String type) {
-		for (Category c : values()) {
-			if (c.getType().equals(type))
-				return c;
-		}
-		return null;
-	}
+    public static Category getByType(String type) {
+        for (Category c : values()) {
+            if (c.getType().equals(type))
+                return c;
+        }
+        return null;
+    }
 
-	public static List<String> getCategories() {
-		List<String> res = new ArrayList<String>();
-		for (Category c : values()) {
-			res.add(c.getType());
-		}
-		return res;
-	}
+    public static List<String> getCategories() {
+        List<String> res = new ArrayList<String>();
+        for (Category c : values()) {
+            res.add(c.getType());
+        }
+        return res;
+    }
 
 }
