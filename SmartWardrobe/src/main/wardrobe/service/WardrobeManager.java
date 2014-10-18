531eae257a50c880a6cea67872752deb49b84dae
TDiva <tanushadomanova@gmail.com>
2014-10-18 22:36:06 +0300

    fix wardrope

--- SmartWardrobe/src/main/wardrobe/service/WardrobeManager.java
+++ SmartWardrobe/src/main/wardrobe/service/WardrobeManager.java
@@ -10,12 +10,6 @@ import main.wardrobe.repository.ApparelRepositoryCap;
 
 public class WardrobeManager {
 
-    private static List<Apparel> testList = Arrays.asList(
-            new Apparel("Ap1", null, Category.ACCESSORIES, 0, "desc"),
-            new Apparel("Ap2", null, Category.DRESS, 4, "desc"),
-            new Apparel("Ap3", null, Category.SHIRT, 1, "desc")
-    );
-
     private static WardrobeManager instance;
 
     public static WardrobeManager getInstance() {
@@ -29,9 +23,6 @@ public class WardrobeManager {
 
     private WardrobeManager() {
         repository = new ApparelRepositoryCap();
-        for (Apparel ap: testList) {
-            repository.addApparel(ap);
-        }
     }
 
     public List<Apparel> getByCategory(String category) {
