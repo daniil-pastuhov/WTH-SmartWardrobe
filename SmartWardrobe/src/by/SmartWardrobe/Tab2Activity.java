b673277ad42c3f444fd21e68404f1f813fb5d050
Daniil Pastuhov <daniil.pastuhov@gmail.com>
2014-10-18 23:50:23 +0300

    Merge

--- SmartWardrobe/src/by/SmartWardrobe/Tab2Activity.java
+++ SmartWardrobe/src/by/SmartWardrobe/Tab2Activity.java
@@ -30,18 +30,27 @@ public class Tab2Activity  extends Activity
 
         final ArrayAdapter<String> adapterMain = new ArrayAdapter<String>(this, by.idea.SmartWardrobe.R.layout.list_item_category, Category.getCategories());
         final ListView lst = (ListView) findViewById(by.idea.SmartWardrobe.R.id.list_of_categories);
+
         lst.setAdapter(adapterMain);
+        final List<Apparel> emptyList = new ArrayList<Apparel>();
+        final Bitmap myBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.backspace);
+        final Apparel backing = new Apparel("...", myBitmap, Category.OTHER, 0, "...");
+        emptyList.add(0, backing);
+        final ListViewAdapter adapterEmpty = new ListViewAdapter(activity, R.layout.listview_item, emptyList);
         lst.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 if (isMainMenu) {
                     List<Apparel> tempList = WardrobeManager.getInstance().getByCategory(adapterView.getAdapter().getItem(i).toString());
-                    Bitmap myBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.backspace);
-                    if (tempList == null)
-                        tempList = new ArrayList<Apparel>();
-                    tempList.add(0, new Apparel("...", myBitmap, Category.OTHER, 0, "..." ));
-                    ListViewAdapter adapter = new ListViewAdapter(activity, R.layout.listview_item, tempList);
-                    lst.setAdapter(adapter);
+                    if (tempList.size() == 0) {
+                        lst.setAdapter(adapterEmpty);
+                    }
+                    else{
+                        if (!tempList.get(0).equals(backing))
+                            tempList.add(0, backing);
+                        ListViewAdapter adapter = new ListViewAdapter(activity, R.layout.listview_item, tempList);
+                        lst.setAdapter(adapter);
+                    }
                     isMainMenu = false;
                 }
                 else {
