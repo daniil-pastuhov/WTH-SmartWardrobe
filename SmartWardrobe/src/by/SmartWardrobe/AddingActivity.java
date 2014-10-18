ab9017dedc4358717763956f014723a0cae7989c
Nastassia Mitskevich <nastassia.mitskevich@gmail.com>
2014-10-18 22:25:03 +0300

    Adding activity edited.

--- SmartWardrobe/src/by/SmartWardrobe/AddingActivity.java
+++ SmartWardrobe/src/by/SmartWardrobe/AddingActivity.java
@@ -3,7 +3,14 @@ package by.SmartWardrobe;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+
 import by.idea.SmartWardrobe.R;
+import main.constants.Category;
 
 public class AddingActivity extends Activity {
     @Override
@@ -12,6 +19,8 @@ public class AddingActivity extends Activity {
         setContentView(R.layout.adding_activity);
         //TODO сделать добавление предмета в коллекцию + вызов камеры!!!
 
-        startActivity(new Intent(Intent.ACTION_CAMERA_BUTTON));
+        //startActivity(new Intent(Intent.ACTION_CAMERA_BUTTON));
+
+
     }
 }
